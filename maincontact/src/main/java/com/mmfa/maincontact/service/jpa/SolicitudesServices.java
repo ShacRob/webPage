12d539497e58d5d0c94b5fa4703b9fabb6bb381f
ShacRob <isunixg@gmail.com>
2020-07-08 23:34:04 -0500

    contacto

--- maincontact/src/main/java/com/mmfa/maincontact/service/jpa/SolicitudesServices.java
+++ maincontact/src/main/java/com/mmfa/maincontact/service/jpa/SolicitudesServices.java
@@ -6,15 +6,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mmfa.maincontact.entity.Asociado;
+import com.mmfa.maincontact.entity.Correo;
 import com.mmfa.maincontact.entity.Solicitud;
 import com.mmfa.maincontact.repository.SolicitudesRepository;
+import com.mmfa.maincontact.service.ICorreosServices;
 import com.mmfa.maincontact.service.ISolicitudesServices;
+import com.mmfa.maincontact.service.util.EnvioEmailServices;
 
 @Service
 public class SolicitudesServices implements ISolicitudesServices {
 
-	@Autowired
-	private SolicitudesRepository repository;
+	@Autowired private SolicitudesRepository repository;
+	@Autowired private ICorreosServices serviceCorreo;
+	@Autowired private EnvioEmailServices serviceEnvio;
 
 	@Override
 	public List<Solicitud> buscarTodo() {
@@ -23,6 +28,10 @@ public class SolicitudesServices implements ISolicitudesServices {
 
 	@Override
 	public Solicitud guardar(Solicitud obj) {
+		Correo correo = serviceCorreo.buscar(obj.getAsociado().getId());
+		String subject = "Contactar a "+obj.getNombre();
+		String contenido = obj.getText() +"\n\n Datos de contacto: " + "\nNombre: " + obj.getNombre() + "\nE-mail: " + obj.getCorreo()+"\nTelefono: " +obj.getTelefono();
+		serviceEnvio.sendEmail(correo.getCorreo(), subject, contenido);
 		return repository.save(obj);
 	}
 
